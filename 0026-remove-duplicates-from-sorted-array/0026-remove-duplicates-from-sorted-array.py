00ca365054f9eaf247710bf12e193180c09726c2
AX1S <apk050406@gmail.com>
2026-09-17 14:17:05 +0530

    Time: 59 ms (5.14%), Space: 20.5 MB (43.27%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,8 +1,9 @@
-class Solution:
-    def removeDuplicates(self, nums: list[int]) -> int:
-        seen=set()
-        for i in range(len(nums)):
-            if nums[i] not in seen:
-                seen.add(nums[i])
-            else:
+class Solution(object):
+    def removeDuplicates(self, nums):
+        i = 1
+        while i < len(nums):
+            if nums[i] == nums[i-1]:
                 nums.pop(i)
+            else:
+                i += 1
+        return len(nums)
